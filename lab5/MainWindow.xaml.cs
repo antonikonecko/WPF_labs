6e9e76cf6018da7a185a990076c4c88c2bff0d08
antonikonecko <antekx17@gmail.com>
2021-11-15 19:08:30 +0100

    new class Sequence

--- lab5/MainWindow.xaml.cs
+++ lab5/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,14 +26,34 @@ namespace lab5
             InitializeComponent();
         }
 
+        static readonly int kmer_length = 4;
+        private Sequence seq;
         private void load_seq_btn_Click(object sender, RoutedEventArgs e)
         {
+            string filePath = string.Empty;
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.InitialDirectory = "c:\\";
+            openfiledialog.Filter = "fasta files (*.fasta)|*.fasta|txt files (*.txt)|*.txt|all files (*.*)|*.*";
+            openfiledialog.RestoreDirectory = true;
 
+            bool? userClickedOK = openfiledialog.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == true)
+            {
+                filePath = openfiledialog.FileName;
+                // {Binding} .Clear();
+                string inputseq = System.IO.File.ReadAllText(filePath);
+                seq = new Sequence(inputseq);
+                seq_txtbox.Text = seq.Get_sequence();
+                subseq_count_txtbox.Text = seq.Get_count().ToString();
+            }
         }
 
         private void find_pattern_btn_Click(object sender, RoutedEventArgs e)
         {
 
+            seq.PatternFind(kmer_length);
         }
 
         private void cbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
---
+++ lab5/Sequence.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    internal class Sequence
+    {
+        private int count = 0;
+        
+        private String sequence = string.Empty;
+        public String pattern = string.Empty;
+        
+        public Sequence(String seq) 
+        {
+            sequence = seq;
+            count = Get_count();            
+        }
+        public int Get_count()
+        { 
+            return count;
+        }
+        public String Get_sequence()
+        { 
+            return sequence;
+        }
+
+        private String FindPattern() 
+        {
+
+            return pattern; 
+        }
+
+        public String[] PatternFind(int k)
+        {
+            string text = sequence;
+            int[] Count = new int[text.Length];
+            String[] frequent_patterns = new String[] {};
+            
+            for (int i = 0; i < text.Length - k; i++)
+            {
+                pattern = text.Substring(i, k);
+                Count[i] = PatternCount(text, pattern); // Algorytm 1
+            }
+            int maxCount = Count.Max();
+            for (int i = 0; i < text.Length - k; i++)
+            {
+                if (Count[i] == maxCount)
+                {
+                    frequent_patterns = (string[])frequent_patterns.Append(text.Substring(i, k));
+                }               
+            }
+
+            frequent_patterns = frequent_patterns.Distinct().ToArray();
+            //usuń duplikaty z FrequentPatterns
+
+            return frequent_patterns;            
+        }
+
+        private int PatternCount(String text, String pattern)
+        {
+            for (int i = 0; i < (text.Length - pattern.Length); i++)
+            {
+                if (text.Substring(i, pattern.Length) == pattern)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
