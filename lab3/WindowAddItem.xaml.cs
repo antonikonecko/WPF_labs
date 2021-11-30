8cabaf69dc2da3f7701e38d985df67f6a7f6f250
antonikonecko <61695517+antonikonecko@users.noreply.github.com>
2021-12-01 00:14:36 +0100

    Add files via upload

---
+++ lab3/WindowAddItem.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window2.xaml
+    /// </summary>
+    public partial class WindowAddItem : Window
+    {
+        public bool ChkTextChanged;
+        public WindowAddItem( bool ChkTextChanged)        {
+            
+            InitializeComponent();
+            this.ChkTextChanged = ChkTextChanged;
+        }
+        
+        private void TextBoxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ChkTextChanged = true;
+        }
+    }
+}
