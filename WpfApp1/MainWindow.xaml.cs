ee9bd7a4abe2d2f23c3a675f8eaf8a81a8721140
antonikonecko <antekx17@gmail.com>
2022-01-04 20:24:52 +0100

    odszyfrowywanie, walidacja hasła, zwiekszenie ilosci iteracji i wielkosci klucza AES

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace lab11
@@ -11,8 +12,13 @@ namespace lab11
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {      
-        string b64_encrypted_data;
+    {        
+        string b64_encrypted_data;        
+        byte[] aes_iv;
+        int myIterations = 1000000;
+        byte[] salt2;
+        Rfc2898DeriveBytes PBKDF2_key;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -20,7 +26,7 @@ namespace lab11
 
         private void btn_encrypt_Click(object sender, RoutedEventArgs e)
         {
-            if (tb_text.Text != String.Empty && pb_password.Password != String.Empty)
+            if (tb_text.Text != String.Empty && ValidatePassword(pb_password.Password))
             {
                 string password = pb_password.Password;
                 string data_to_encrypt = tb_text.Text;
@@ -31,34 +37,115 @@ namespace lab11
                     // Fill the array with a random value.
                     rng.GetBytes(salt);
                 }
-                int myIterations = 1000;
-
-                Rfc2898DeriveBytes key = new(password, salt, myIterations);
+                
+                PBKDF2_key = new(password, salt, myIterations);                
+                salt2 = salt;
 
                 // Encrypt the data.
                 Aes encAlg = Aes.Create();
-                encAlg.Key = key.GetBytes(16);
-
+                encAlg.Key = PBKDF2_key.GetBytes(32);                
                 MemoryStream encryptionStream = new MemoryStream();
                 CryptoStream encrypt = new CryptoStream(encryptionStream, encAlg.CreateEncryptor(), CryptoStreamMode.Write);
-                byte[] bytes = new UTF8Encoding(false).GetBytes(data_to_encrypt);
-
+                byte[] bytes = new UTF8Encoding(false).GetBytes(data_to_encrypt);                
                 encrypt.Write(bytes, 0, bytes.Length);
                 encrypt.FlushFinalBlock();
-                encrypt.Close();
-
+                encrypt.Close();               
                 byte[] encrypted_data = encryptionStream.ToArray();
+
                 b64_encrypted_data = Convert.ToBase64String(encrypted_data);
                 tb_encrypted.Text = b64_encrypted_data;
-                key.Reset();
+               
+                //string hex_key = BitConverter.ToString(encAlg.Key);
+                //string hex_iv = BitConverter.ToString(encAlg.IV);
+                //tb_key.Text = hex_key;
+                //tb_iv.Text = hex_iv;
+                
+                aes_iv = encAlg.IV;
+                PBKDF2_key.Reset();
+                pb_password.Clear();
             }
             else
-            {                
-                b64_encrypted_data = String.Empty;
+            {
+                pb_password.Clear();
+                b64_encrypted_data = String.Empty;                
                 tb_encrypted.Text = b64_encrypted_data;
                 MessageBox.Show("Podaj hasło i/lub tekst!");
-            }               
-            
+            }                           
+        }
+        private void btn_decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(pb_password.Password))
+            {
+                pb_password.Clear();
+                MessageBox.Show("Wpisz hasło");
+            }
+            else
+            {
+                Rfc2898DeriveBytes PBKDF2_key2 = new(pb_password.Password, salt2, myIterations);
+                
+                pb_password.Clear();
+                Aes decAlg = Aes.Create();
+                decAlg.IV = aes_iv;
+                decAlg.Key = PBKDF2_key2.GetBytes(32);
+
+                byte[] encrypted_data = Convert.FromBase64String(b64_encrypted_data);
+                MemoryStream decryptionStreamBacking = new MemoryStream();
+                CryptoStream decrypt = new CryptoStream(decryptionStreamBacking, decAlg.CreateDecryptor(), CryptoStreamMode.Write);
+                decrypt.Write(encrypted_data, 0, encrypted_data.Length);
+                decrypt.Flush();
+                decrypt.Close();
+                string decrypted_data = new UTF8Encoding(false).GetString(decryptionStreamBacking.ToArray());
+                tb_decrypted.Text = decrypted_data;
+                PBKDF2_key2.Reset();
+                
+            }         
+        }
+
+        private bool ValidatePassword(string password)
+        {
+
+            var input = password;            
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Password should not be empty");
+            }
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMiniMaxChars = new Regex(@".{8,}");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+            if (!hasLowerChar.IsMatch(input))
+            {
+                MessageBox.Show("Password should contain at least one lower case letter.");
+                return false;
+            }
+            else if (!hasUpperChar.IsMatch(input))
+            {
+                MessageBox.Show("Password should contain at least one upper case letter.");
+                return false;
+            }
+            else if (!hasMiniMaxChars.IsMatch(input))
+            {
+                MessageBox.Show("Password should not be lesser than 8 characters.");
+                return false;
+            }
+            else if (!hasNumber.IsMatch(input))
+            {
+                MessageBox.Show("Password should contain at least one numeric value.");
+                return false;
+            }
+
+            else if (!hasSymbols.IsMatch(input))
+            {
+                MessageBox.Show("Password should contain at least one special case character.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         private void btn_save_Click(object sender, RoutedEventArgs e)
@@ -66,15 +153,18 @@ namespace lab11
             if(b64_encrypted_data != String.Empty)
             {
                 SaveFileDialog savefile = new SaveFileDialog();
+                savefile.Filter = "Text Files | *.txt";
+                savefile.DefaultExt = "txt";
                 if (savefile.ShowDialog() ?? true)
                     using (StreamWriter writer = new StreamWriter(savefile.FileName))
                     {
                         writer.WriteLine(b64_encrypted_data);
                     }
+                MessageBox.Show("Zapisano");
             }
             else 
                 MessageBox.Show("Najpierw zaszyfruj tekst!");
-        }
+        }        
     }
 }
 
